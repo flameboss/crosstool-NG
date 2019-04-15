6d1d7c16838e97de9ff4fb9d3736078f8af9bbc1
Roger Collins <roger@flameboss.com>
2019-04-15 12:58:02 -0400

    fix cloog url

--- scripts/build/companion_libs/130-cloog.sh
+++ scripts/build/companion_libs/130-cloog.sh
@@ -25,7 +25,7 @@ cloog_basename_version() {
 # Download CLooG
 do_cloog_get() {
     CT_GetFile "$(cloog_basename_version)"          \
-        https://s8.dev.flameboss.com \
+        https://myflameboss.com \
         ftp://gcc.gnu.org/pub/gcc/infrastructure
 }
 
